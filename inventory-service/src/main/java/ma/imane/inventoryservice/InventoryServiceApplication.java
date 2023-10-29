a96cc822a158d3a30df65a02b75180ed9eff4976
loukili-imane <imane.loukili2115@gmail.com>
2023-10-29 19:23:43 +0100

    test product crud operations with commandLineRunner

--- inventory-service/src/main/java/ma/imane/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/ma/imane/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package ma.imane.inventoryservice;
 
+import ma.imane.inventoryservice.entities.Product;
+import ma.imane.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,17 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository){
+		return args -> {
+			productRepository.save(new Product(null,"Ordinateur",5000,10));
+			productRepository.save(new Product(null,"souris",70,15));
+			productRepository.save(new Product(null,"clavier",80,20));
+			productRepository.findAll().forEach(product -> {
+				System.out.println(product.toString());
+			});
+
+
+		};
+	}
 }
