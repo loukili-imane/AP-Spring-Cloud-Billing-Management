9b4251cdc1694b6284fbd6be288a519ae52c08e7
loukili-imane <imane.loukili2115@gmail.com>
2023-10-30 05:43:31 +0100

    enable Dynamic routes configuration with Discovery Service

--- gateway/src/main/java/ma/imane/gateway/GatewayApplication.java
+++ gateway/src/main/java/ma/imane/gateway/GatewayApplication.java
@@ -2,9 +2,9 @@ package ma.imane.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -14,11 +14,9 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	//Dynamic routes configuration with Discovery Service
 	@Bean
-	RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
-		return routeLocatorBuilder.routes()
-				.route("r1",(r)->r.path("/customers/**").uri("http://localhost:8081/"))
-				.route("r2",(r)->r.path("/products/**").uri("http://localhost:8082/"))
-				.build();
-	}
-}
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+														DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}}
