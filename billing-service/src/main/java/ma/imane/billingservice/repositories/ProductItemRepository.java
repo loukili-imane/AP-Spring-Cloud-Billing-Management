26cf33cc452f8146bda5aa81e9769fde77e22608
loukili-imane <imane.loukili2115@gmail.com>
2023-10-30 06:28:10 +0100

    add findById in ProductItemRepository

--- billing-service/src/main/java/ma/imane/billingservice/repositories/ProductItemRepository.java
+++ billing-service/src/main/java/ma/imane/billingservice/repositories/ProductItemRepository.java
@@ -4,6 +4,10 @@ import ma.imane.billingservice.entities.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Collection;
+
 @RepositoryRestResource
 public interface ProductItemRepository extends JpaRepository <ProductItem,Long> {
+    public Collection<ProductItem> findById(long id);
+
 }
